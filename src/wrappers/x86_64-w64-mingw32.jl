c3bc043d19b8572dc5e8bdb930574a034d13c2b8
jlbuild <juliabuildbot@gmail.com>
2023-03-30 19:03:27 +0000

    CyrusSASL_jll build 2.1.28+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -17,49 +17,49 @@ function __init__()
     JLLWrappers.@generate_init_header(OpenSSL_jll, SQLite_jll, Gdbm_jll)
     JLLWrappers.@init_library_product(
         libanonymous,
-        "lib/sasl2/libanonymous-3.dll",
+        "lib\\sasl2\\libanonymous-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libcrammd5,
-        "lib/sasl2/libcrammd5-3.dll",
+        "lib\\sasl2\\libcrammd5-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libdigestmd5,
-        "lib/sasl2/libdigestmd5-3.dll",
+        "lib\\sasl2\\libdigestmd5-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libntlm,
-        "lib/sasl2/libntlm-3.dll",
+        "lib\\sasl2\\libntlm-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libotp,
-        "lib/sasl2/libotp-3.dll",
+        "lib\\sasl2\\libotp-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libplan,
-        "lib/sasl2/libplain-3.dll",
+        "lib\\sasl2\\libplain-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsasl2,
-        "bin/libsasl2-3.dll",
+        "bin\\libsasl2-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libscram,
-        "lib/sasl2/libscram-3.dll",
+        "lib\\sasl2\\libscram-3.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
